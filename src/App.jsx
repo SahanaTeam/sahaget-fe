12825abb084a14a08e49aeae796eac7753c56b50
AbuuIbtisaam <mansoooormustapha22@gmail.com>
2023-10-20 13:08:21 +0530

    Bootstrap Set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Footer from "./components/footer";
+import Dropdown from "./components/dropdown";
 function App() {
-  return <h1>Sahaget</h1>;
+  return (
+  <>
+    <div className="App row">      
+      <div className="col-12">
+        <div className="text-center">
+          <h1>Report what you found or lost</h1>
+          <br/>
+          <p>
+            <a href="#" className="btn btn-success btn-lg">Get Started</a>
+          </p>
+          <Dropdown></Dropdown>
+        </div>                  
+      </div>
+    </div>
+    <Footer></Footer>
+  </>
+  );
 }
 
 export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return(        
+        <div className="row">
+            <div className="col text-center bg-success py-3 text-light">
+            &copy;SahanaTechTeam - 2023
+        </div>
+        </div>
+    );
+}
+
+
+export default Footer;
