653e8c46b4147f28320b4fdfb544246beda6370f
leandre <tuyambazeleandre@gmail.com>
2023-10-22 12:50:16 +0200

    setup husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-unused-vars': ['error', { varsIgnorePattern: '^React', argsIgnorePattern: '^_' }],
+    'import/no-unused-modules': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-param-reassign': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+}
